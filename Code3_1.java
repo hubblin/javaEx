7912aadc101426d5b3976b7dc510f3a6de6c5569
hubblin <43435547+hubblin@users.noreply.github.com>
2020-07-03 23:34:06 +0900

    Add Conde3_1.java

--- Code3_1.java
+++ Code3_1.java
@@ -21,15 +21,18 @@ public class Code3_1 {
         for(int i = n-1; i>0; i--){
             for(int j=0; j <i; j++){
                 if(data[j] > data[j+1]){
-                    swap(data[j], data[j+1]);
+                    //swap(data[j], data[j+1]);
+                    int temp = data[j];
+                    data[j] = data[j+1];
+                    data[j+1] = temp;
                 }
             }
         }
     }
 
-    public static void swap(int a, int b){
+/*    public static void swap(int a, int b){
         int tmp = a;
         a = b;
         b = tmp;
-    }
+    }*/
 }
