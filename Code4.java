86078de02bb22f8413e2a3963a71b84179135d33
hubblin <43435547+hubblin@users.noreply.github.com>
2020-07-05 11:40:10 +0900

    Add Code4.java

--- Code4.java
+++ Code4.java
@@ -7,18 +7,29 @@ import java.util.Scanner;
 public class Code4 {
     public static void main(final String[] args) {
 
+        String[] name = new String[1000];
+        String[] number = new String[1000];
+        int m = 0;
+
         // Scanner inFile = new Scanner(Code4.class.getResourceAsStream("input.txt"));
-        Scanner inFile;
         try {
-            inFile = new Scanner(new File("D:\\java remind\\code16\\javaEx\\input.txt"));
-            String name = inFile.next();
-
-
+            Scanner inFile = new Scanner(new File("D:\\java remind\\code16\\javaEx\\input.txt"));
+            
+            while(inFile.hasNext()){
+                name[m] = inFile.next();
+                number[m] = inFile.next();
+                m++;
+            }
+            
+            
             inFile.close();
         } catch (FileNotFoundException e) {
             // TODO Auto-generated catch block
             System.out.println("No File");
             return;
+        }
+        for(int i=0; i<m;i++){
+            System.out.println(name[i] + ":" + number[i]);
         }
     
     }
